18e514a2ee994241e888fc4703c9a73ee9f9049f
midhun98 <midhunskani@gmail.com>
2023-07-26 23:09:44 +0530

    Created a form to add marketplace

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from core.models import person_collection, marketplace_collection
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
+import base64
 
 def index(request):
     return HttpResponse("<h1>app is running</h1>")
@@ -22,10 +23,25 @@ def get_all_person(request):
 @csrf_exempt
 @api_view(['POST'])
 def create_marketplace_api(request):
-    records = {
-        "name": request.data['name'],
-        "logo": request.data['logo']
-    }
-    marketplace_collection.insert_one(records)
-    return HttpResponse("Marketplace added")
+    try:
+        name = request.data['name']
+        logo = request.data['logo']
+
+        if logo:
+            logo_base64 = base64.b64encode(logo.read()).decode('utf-8')
+        else:
+            logo_base64 = None
+
+        records = {
+            "name": name,
+            "logo": logo_base64
+        }
+
+        marketplace_collection.insert_one(records)
+        print("done")
+        return HttpResponse("Marketplace added")
+    except Exception as e:
+        print("Error:", e)
+        return HttpResponse("Error adding Marketplace", status=500)
+
 
--- frontEnd/urls.py
+++ frontEnd/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("create_marketplace", TemplateView.as_view(template_name="create_marketplace.html"), name='index'),
+    path("create_marketplace/", TemplateView.as_view(template_name="create_marketplace.html"), name='create-marketplace'),
     path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='dashboard'),
 ]
